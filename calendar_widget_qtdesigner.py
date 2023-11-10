46b8bbdc04a090e2c54229acfd672a8ae68405db
Dokukin Dmitry <72608150+l-m-n-d-k@users.noreply.github.com>
2023-11-10 04:28:23 +0300

    Get_event_by_data and update add_event

--- calendar_widget_qtdesigner.py
+++ calendar_widget_qtdesigner.py
@@ -2,17 +2,22 @@ from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5.uic import loadUi
 import sys
 
+events = ["Write email", 'LMS']
+
 class CalendarWindow(QWidget):
     def __init__(self):
         super().__init__()
         loadUi('calendar.ui', self)
         self.setFixedSize(840, 400)
         self.calendarWidget.selectionChanged.connect(self.calendarDateChanged)
-
+        
     def calendarDateChanged(self):
         dateSelected = self.calendarWidget.selectedDate().toPyDate()
-        print('Date selected:', dateSelected)
         return dateSelected
+    
+    def updateEventList(self):
+        for event in events:
+            ...
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- google_api_calendar.py
+++ google_api_calendar.py
@@ -3,8 +3,8 @@ from googleapiclient.discovery import build
 
 
 class GoogleCalendar:
-    SCOPES = ['https://www.googleapis.com/auth/calendar']
-    FILE_PATH = 'grand-appliance-403520-b06ed9fdb088.json'
+    SCOPES = ["https://www.googleapis.com/auth/calendar"]
+    FILE_PATH = "grand-appliance-403520-b06ed9fdb088.json"
 
     def __init__(self):
         # Определение областей видимости и .json-файла для учетной записи службы google
@@ -12,31 +12,56 @@ class GoogleCalendar:
             filename=self.FILE_PATH, scopes=self.SCOPES
         )
         # Авторизация в учетной записи службы google
-        self.service = build('calendar', 'v3', credentials=credentials)
-    
+        self.service = build("calendar", "v3", credentials=credentials)
+
     # Список календарей
     def get_calendar_list(self):
         return self.service.calendarList().list().execute()
-    
+
     # Создание календаря
     def add_calendar(self, calendar_id):
-        calendar_list_entry = {
-            'id': calendar_id
-        } 
-        return self.service.calendarList().insert(
-            body=calendar_list_entry
-            ).execute()
-    
+        calendar_list_entry = {"id": calendar_id}
+        return self.service.calendarList().insert(body=calendar_list_entry).execute()
+
+    # Получение списка событий на выбранную дату
+    def get_event_by_date(self, calendar_id, date):
+        events_result = (
+            self.service.events()
+            .list(
+                calendarId=calendar_id,
+                timeMin=date + "T00:00:00Z",
+                timeMax=date + "T23:59:59Z",
+            )
+            .execute()
+        )
+        return(events_result.get("items", []))
+
+
     # Добавление события
-    def add_event(self, calendar_id, event):
-        return self.service.events().insert(
-            calendarId=calendar_id, 
-            body=event).execute()        
+    def add_event(self, calendar_id, summary, location=None, description=None, start_dateTime=None, end_dateTime=None):
+        event = {
+            "summary": summary,
+            "location": location,
+            "description": description,
+            "start": {
+                'dateTime': start_dateTime,
+                'timeZone': 'Europe/Moscow',
+            },  
+            "end": {
+                'dateTime': end_dateTime,
+                'timeZone': 'Europe/Moscow',
+            },
+        }
+
+        return (
+            self.service.events().insert(calendarId=calendar_id, body=event).execute()
+        )
 
     # Удаление события
     def delete_evente(self, calendar_id, event_name):
         events_result = self.service.events().list(calendarId=calendar_id).execute()
-        events = events_result.get('items', [])
+        events = events_result.get("items", [])
+        print(events)
 
         # Поиск события по названию
         for event in events:
@@ -47,23 +72,11 @@ class GoogleCalendar:
                 return
             print(f'Событие "{event_name}" не найдено')
 
-# obj = GoogleCalendar()
 
-# calendar_id = 'dima2642007@gmail.com'
-# event = {
-#   'summary': 'Тестовый пример',
-#   'location': 'Москва',
-#   'description': 'Тестовое описание',
-#   'start': {
-#     'date': '2023-11-1',
-#   },
-#   'end': {
-#     'date': '2023-11-4',
-#   },
-# }
+obj = GoogleCalendar()
 
-# # Добавление события
-# # obj.add_event(calendar_id=calendar_id, event=event)
+calendar_id = "dima2642007@gmail.com"
 
-# # Удаление события
-# # obj.delete_evente(calendar_id=calendar_id, event_name='Тестовый пример').
+events = obj.get_event_by_date(calendar_id, '2023-11-10')
+for event in events:
+    print(event["summary"], event["start"], event["end"])
